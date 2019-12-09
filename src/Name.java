efe1a6acae94f2d426d595846d467d8fa01927a5
jhwang37 <jhwang37@gmail.com>
2019-12-09 10:30:25 -0500

    initial commit

---
+++ src/Name.java
@@ -0,0 +1,9 @@
+public class Name {
+    public static void main(String[] args) {
+        String myVar = "James";
+
+        System.out.println("Hello World!");
+        System.out.println(myVar);
+        System.out.println("Hello and welcome " + myVar + ".");
+    }
+}
